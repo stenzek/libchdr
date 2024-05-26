d1fe3009c8b025c49629d12b27ec16bea2cfd77d
Romain TISSERAND <romain.tisserand@gmail.com>
2024-05-26 20:03:35 +0200

    Revert "Merge libretro stuff, bump to 0.3 version"
    This reverts commit 56b08d53f8f9aae900f44ad0e0ae2401eb53403c.

--- include/libchdr/coretypes.h
+++ include/libchdr/coretypes.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#ifdef __LIBRETRO__
-#include <streams/file_stream.h>
+#ifdef USE_LIBRETRO_VFS
+#include <streams/file_stream_transforms.h>
 #endif
 
 #define ARRAY_LENGTH(x) (sizeof(x)/sizeof(x[0]))
@@ -29,39 +29,6 @@ typedef int32_t INT32;
 typedef int16_t INT16;
 typedef int8_t INT8;
 
-#ifdef __LIBRETRO__
-#define core_file RFILE
-#define core_fopen(file) rfopen(file, "rb")
-#define core_fseek rfseek
-#define core_ftell rftell
-#define core_fread(fc, buff, len) rfread(buff, 1, len, fc)
-#define core_fclose rfclose
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-RFILE* rfopen(const char *path, const char *mode);
-extern int64_t rfseek(RFILE* stream, int64_t offset, int origin);
-extern int64_t rftell(RFILE* stream);
-extern int64_t rfread(void* buffer,
-   size_t elem_size, size_t elem_count, RFILE* stream);
-extern int rfclose(RFILE* stream);
-
-#ifdef __cplusplus
-}
-#endif
-
-static INLINE UINT64 core_fsize(core_file *f)
-{
-    UINT64 rv;
-    UINT64 p = core_ftell(f);
-    core_fseek(f, 0, SEEK_END);
-    rv = core_ftell(f);
-    core_fseek(f, p, SEEK_SET);
-    return rv;
-}
-#else
 typedef struct chd_core_file {
 	/*
 	 * arbitrary pointer to data the implementation uses to implement the below functions
@@ -107,6 +74,5 @@ static inline UINT64 core_fsize(core_file *fp)
 {
 	return fp->fsize(fp);
 }
-#endif
 
 #endif
